0bf33e6facac4930feb770ed6b51d78ad2c5d256
Felix Fischer <fischerfelix06@gmail.com>
2025-01-01 17:49:38 +0100

    update base for MPU6500

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 /***************************************************************************
-* Example sketch for the MPU9250_WE library
+* Example sketch for the MPU6500_WE library
 *
-* This sketch shows how to get acceleration, gyroscocope, magnetometer and 
-* temperature data from the MPU9250. It might be a bit confusing with all the 
-* settings. Therefore you will also find sketches for the individual data.
+* This sketch shows how to get acceleration, gyroscocope and temperature 
+* data from the MPU6500. In essence, the difference to the MPU9250 is the
+* missing magnetometer. The shall only show how to "translate" all other 
+* MPU9250 example sketches for use of the MPU6500
 * 
 * For further information visit my blog:
 *
@@ -12,47 +13,41 @@
 * 
 ***************************************************************************/
 
-#include <MPU9250_WE.h>
+#include <MPU6500_WE.h>
 #include <Wire.h>
-#define MPU9250_ADDR 0x68
+#define MPU6500_ADDR 0x68
 
-/* There are several ways to create your MPU9250 object:
- * MPU9250_WE myMPU9250 = MPU9250_WE()              -> uses Wire / I2C Address = 0x68
- * MPU9250_WE myMPU9250 = MPU9250_WE(MPU9250_ADDR)  -> uses Wire / MPU9250_ADDR
- * MPU9250_WE myMPU9250 = MPU9250_WE(&wire2)        -> uses the TwoWire object wire2 / MPU9250_ADDR
- * MPU9250_WE myMPU9250 = MPU9250_WE(&wire2, MPU9250_ADDR) -> all together
+/* There are several ways to create your MPU6500 object:
+ * MPU6500_WE myMPU6500 = MPU6500_WE()              -> uses Wire / I2C Address = 0x68
+ * MPU6500_WE myMPU6500 = MPU6500_WE(MPU6500_ADDR)  -> uses Wire / MPU6500_ADDR
+ * MPU6500_WE myMPU6500 = MPU6500_WE(&wire2)        -> uses the TwoWire object wire2 / MPU6500_ADDR
+ * MPU6500_WE myMPU6500 = MPU6500_WE(&wire2, MPU6500_ADDR) -> all together
  * Successfully tested with two I2C busses on an ESP32
  */
-MPU9250_WE myMPU9250 = MPU9250_WE(MPU9250_ADDR);
+MPU6500_WE myMPU6500 = MPU6500_WE(MPU6500_ADDR);
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
-  if(!myMPU9250.init()){
-    Serial.println("MPU9250 does not respond");
+  if(!myMPU6500.init()){
+    Serial.println("MPU6500 does not respond");
   }
   else{
-    Serial.println("MPU9250 is connected");
+    Serial.println("MPU6500 is connected");
   }
-  if(!myMPU9250.initMagnetometer()){
-    Serial.println("Magnetometer does not respond");
-  }
-  else{
-    Serial.println("Magnetometer is connected");
-  }
-
+  
   /* The slope of the curve of acceleration vs measured values fits quite well to the theoretical 
    * values, e.g. 16384 units/g in the +/- 2g range. But the starting point, if you position the 
-   * MPU9250 flat, is not necessarily 0g/0g/1g for x/y/z. The autoOffset function measures offset 
-   * values. It assumes your MPU9250 is positioned flat with its x,y-plane. The more you deviate 
+   * MPU6500 flat, is not necessarily 0g/0g/1g for x/y/z. The autoOffset function measures offset 
+   * values. It assumes your MPU6500 is positioned flat with its x,y-plane. The more you deviate 
    * from this, the less accurate will be your results.
    * The function also measures the offset of the gyroscope data. The gyroscope offset does not   
    * depend on the positioning.
    * This function needs to be called at the beginning since it can overwrite your settings!
    */
-  Serial.println("Position you MPU9250 flat and don't move it - calibrating...");
+  Serial.println("Position you MPU6500 flat and don't move it - calibrating...");
   delay(1000);
-  myMPU9250.autoOffsets();
+  myMPU6500.autoOffsets();
   Serial.println("Done!");
   
   /*  This is a more accurate method for calibration. You have to determine the minimum and maximum 
@@ -60,26 +55,26 @@ void setup() {
    *  You call the function as follows: setAccOffsets(xMin,xMax,yMin,yMax,zMin,zMax);
    *  Use either autoOffset or setAccOffsets, not both.
    */
-  //myMPU9250.setAccOffsets(-14240.0, 18220.0, -17280.0, 15590.0, -20930.0, 12080.0);
+  //myMPU6500.setAccOffsets(-14240.0, 18220.0, -17280.0, 15590.0, -20930.0, 12080.0);
 
-  /*  The gyroscope data is not zero, even if you don't move the MPU9250. 
+  /*  The gyroscope data is not zero, even if you don't move the MPU6500. 
    *  To start at zero, you can apply offset values. These are the gyroscope raw values you obtain
    *  using the +/- 250 degrees/s range. 
    *  Use either autoOffset or setGyrOffsets, not both.
    */
-  //myMPU9250.setGyrOffsets(45.0, 145.0, -105.0);
+  //myMPU6500.setGyrOffsets(45.0, 145.0, -105.0);
 
   /*  You can enable or disable the digital low pass filter (DLPF). If you disable the DLPF, you 
    *  need to select the bandwdith, which can be either 8800 or 3600 Hz. 8800 Hz has a shorter delay,
    *  but higher noise level. If DLPF is disabled, the output rate is 32 kHz.
-   *  MPU9250_BW_WO_DLPF_3600 
-   *  MPU9250_BW_WO_DLPF_8800
+   *  MPU6500_BW_WO_DLPF_3600 
+   *  MPU6500_BW_WO_DLPF_8800
    */
-  myMPU9250.enableGyrDLPF();
-  //myMPU9250.disableGyrDLPF(MPU9250_BW_WO_DLPF_8800); // bandwdith without DLPF
+  myMPU6500.enableGyrDLPF();
+  //myMPU6500.disableGyrDLPF(MPU6500_BW_WO_DLPF_8800); // bandwdith without DLPF
   
   /*  Digital Low Pass Filter for the gyroscope must be enabled to choose the level. 
-   *  MPU9250_DPLF_0, MPU9250_DPLF_2, ...... MPU9250_DPLF_7 
+   *  MPU6500_DPLF_0, MPU6500_DPLF_2, ...... MPU6500_DPLF_7 
    *  
    *  DLPF    Bandwidth [Hz]   Delay [ms]   Output Rate [kHz]
    *    0         250            0.97             8
@@ -93,36 +88,36 @@ void setup() {
    *    
    *    You achieve lowest noise using level 6  
    */
-  myMPU9250.setGyrDLPF(MPU9250_DLPF_6);
+  myMPU6500.setGyrDLPF(MPU6500_DLPF_6);
 
   /*  Sample rate divider divides the output rate of the gyroscope and accelerometer.
    *  Sample rate = Internal sample rate / (1 + divider) 
    *  It can only be applied if the corresponding DLPF is enabled and 0<DLPF<7!
    *  Divider is a number 0...255
    */
-  myMPU9250.setSampleRateDivider(5);
+  myMPU6500.setSampleRateDivider(5);
 
-  /*  MPU9250_GYRO_RANGE_250       250 degrees per second (default)
-   *  MPU9250_GYRO_RANGE_500       500 degrees per second
-   *  MPU9250_GYRO_RANGE_1000     1000 degrees per second
-   *  MPU9250_GYRO_RANGE_2000     2000 degrees per second
+  /*  MPU6500_GYRO_RANGE_250       250 degrees per second (default)
+   *  MPU6500_GYRO_RANGE_500       500 degrees per second
+   *  MPU6500_GYRO_RANGE_1000     1000 degrees per second
+   *  MPU6500_GYRO_RANGE_2000     2000 degrees per second
    */
-  myMPU9250.setGyrRange(MPU9250_GYRO_RANGE_250);
+  myMPU6500.setGyrRange(MPU6500_GYRO_RANGE_250);
 
-  /*  MPU9250_ACC_RANGE_2G      2 g   (default)
-   *  MPU9250_ACC_RANGE_4G      4 g
-   *  MPU9250_ACC_RANGE_8G      8 g   
-   *  MPU9250_ACC_RANGE_16G    16 g
+  /*  MPU6500_ACC_RANGE_2G      2 g   (default)
+   *  MPU6500_ACC_RANGE_4G      4 g
+   *  MPU6500_ACC_RANGE_8G      8 g   
+   *  MPU6500_ACC_RANGE_16G    16 g
    */
-  myMPU9250.setAccRange(MPU9250_ACC_RANGE_2G);
+  myMPU6500.setAccRange(MPU6500_ACC_RANGE_2G);
 
   /*  Enable/disable the digital low pass filter for the accelerometer 
    *  If disabled the bandwidth is 1.13 kHz, delay is 0.75 ms, output rate is 4 kHz
    */
-  myMPU9250.enableAccDLPF(true);
+  myMPU6500.enableAccDLPF(true);
 
   /*  Digital low pass filter (DLPF) for the accelerometer, if enabled 
-   *  MPU9250_DPLF_0, MPU9250_DPLF_2, ...... MPU9250_DPLF_7 
+   *  MPU6500_DPLF_0, MPU6500_DPLF_2, ...... MPU6500_DPLF_7 
    *   DLPF     Bandwidth [Hz]      Delay [ms]    Output rate [kHz]
    *     0           460               1.94           1
    *     1           184               5.80           1
@@ -133,38 +128,29 @@ void setup() {
    *     6             5              66.96           1
    *     7           460               1.94           1
    */
-  myMPU9250.setAccDLPF(MPU9250_DLPF_6);
+  myMPU6500.setAccDLPF(MPU6500_DLPF_6);
 
   /* You can enable or disable the axes for gyroscope and/or accelerometer measurements.
    * By default all axes are enabled. Parameters are:  
-   * MPU9250_ENABLE_XYZ  //all axes are enabled (default)
-   * MPU9250_ENABLE_XY0  // X, Y enabled, Z disabled
-   * MPU9250_ENABLE_X0Z   
-   * MPU9250_ENABLE_X00
-   * MPU9250_ENABLE_0YZ
-   * MPU9250_ENABLE_0Y0
-   * MPU9250_ENABLE_00Z
-   * MPU9250_ENABLE_000  // all axes disabled
+   * MPU6500_ENABLE_XYZ  //all axes are enabled (default)
+   * MPU6500_ENABLE_XY0  // X, Y enabled, Z disabled
+   * MPU6500_ENABLE_X0Z   
+   * MPU6500_ENABLE_X00
+   * MPU6500_ENABLE_0YZ
+   * MPU6500_ENABLE_0Y0
+   * MPU6500_ENABLE_00Z
+   * MPU6500_ENABLE_000  // all axes disabled
    */
-  //myMPU9250.enableAccAxes(MPU9250_ENABLE_XYZ);
-  //myMPU9250.enableGyrAxes(MPU9250_ENABLE_XYZ);
-  
-  /*
-   * AK8963_PWR_DOWN       
-   * AK8963_CONT_MODE_8HZ         default
-   * AK8963_CONT_MODE_100HZ
-   * AK8963_FUSE_ROM_ACC_MODE 
-   */
-  myMPU9250.setMagOpMode(AK8963_CONT_MODE_100HZ);
+  //myMPU6500.enableAccAxes(MPU6500_ENABLE_XYZ);
+  //myMPU6500.enableGyrAxes(MPU6500_ENABLE_XYZ);
   delay(200);
 }
 
 void loop() {
-  xyzFloat gValue = myMPU9250.getGValues();
-  xyzFloat gyr = myMPU9250.getGyrValues();
-  xyzFloat magValue = myMPU9250.getMagValues();
-  float temp = myMPU9250.getTemperature();
-  float resultantG = myMPU9250.getResultantG(gValue);
+  xyzFloat gValue = myMPU6500.getGValues();
+  xyzFloat gyr = myMPU6500.getGyrValues();
+  float temp = myMPU6500.getTemperature();
+  float resultantG = myMPU6500.getResultantG(gValue);
 
   Serial.println("Acceleration in g (x,y,z):");
   Serial.print(gValue.x);
@@ -181,13 +167,6 @@ void loop() {
   Serial.print(gyr.y);
   Serial.print("   ");
   Serial.println(gyr.z);
-
-  Serial.println("Magnetometer Data in µTesla: ");
-  Serial.print(magValue.x);
-  Serial.print("   ");
-  Serial.print(magValue.y);
-  Serial.print("   ");
-  Serial.println(magValue.z);
 
   Serial.print("Temperature in °C: ");
   Serial.println(temp);
